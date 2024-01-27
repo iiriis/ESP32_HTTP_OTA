e68d01574d30a1cdac57b05d2ab3a2dae0705d89
iiriis <avijitdasxp@gmail.com>
2024-01-27 20:33:25 +0530

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,105 @@
-
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino.h>
 #include <Update.h>
 #include "fw_version.h"
 
+// Replace with your network credentials
 const char* ssid = ":)";
 const char* password = "B003";
 
+// URLs for the firmware .bin and version file hosted on your server
 const char* firmware_url = "<server_IP>:<PORT_NO>/path_to_firmware.bin";
 const char* firmware_ver_url = "<server_IP>:<PORT_NO>/path_to_fw_version.h";
 
 void OTA_UPDATE();
 String check_FW_Version_And_Perform_OTA();
 
-
 void setup() {
   Serial.begin(115200);
   
-  // Connect to Wi-Fi
+  // Connect to Wi-Fi network
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
 
-    Serial.println("Connected to WiFi");
-    Serial.println(WiFi.localIP());
+  // Notify successful Wi-Fi connection and show IP address
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
 
-    check_FW_Version_And_Perform_OTA();
+  // Check firmware version and perform OTA if a new version is found
+  check_FW_Version_And_Perform_OTA();
 }
 
 void loop() {
-    Serial.println("Firmware version " FW_VERSION);
-    delay(1000);
+  // Print the current firmware version; this will run continuously after setup
+  Serial.println("Firmware version " FW_VERSION);
+  delay(1000);
 }
 
-
+// Function to check the firmware version from the server and initiate OTA update if necessary
 String check_FW_Version_And_Perform_OTA()
 {
-  String s;
-    // Perform OTA update
+  String serverVersion;
+  // Create an HTTP client
   HTTPClient http;
-  http.begin(firmware_ver_url);
-  int httpCode = http.GET();
+  http.begin(firmware_ver_url); // Specify the URL for firmware version
+  int httpCode = http.GET(); // Make the request
 
   Serial.println("Current Firmware Version: " FW_VERSION);
 
-  if(httpCode == 200)
+  if(httpCode == 200) // Check the returning status code
   {
-    s = http.getString();
-    s=s.substring(s.indexOf('"')+1, s.lastIndexOf('"'));
+    // Get the response, parse it to get the version number
+    serverVersion = http.getString();
+    serverVersion = serverVersion.substring(serverVersion.indexOf('"')+1, serverVersion.lastIndexOf('"'));
     
-    Serial.println("Firmware Version in server: "+ s);
+    // Print the firmware version from the server
+    Serial.println("Firmware Version in server: "+ serverVersion);
 
-    if(!s.equals(FW_VERSION)){
+    // If the server version is different from the current version, perform OTA update
+    if(!serverVersion.equals(FW_VERSION)){
       Serial.println("Upgrading to Version " FW_VERSION);
       OTA_UPDATE();
     }
   }
-  return s;
+  return serverVersion; // Return the server version string
 }
 
+// Function to perform the OTA update
 void OTA_UPDATE()
 {
-    // Perform OTA update
   HTTPClient http;
-  http.begin(firmware_url);
-  int httpCode = http.GET();
-  Serial.println(httpCode);
+  http.begin(firmware_url); // Specify the firmware binary URL
+  int httpCode = http.GET(); // Make the request
 
-  if (httpCode == 200) {
-    // Get the length of the firmware file
+  // Check the returning status code
+  if (httpCode == 200) { // If file is found on server
+    // Get the size of the firmware file
     int contentLength = http.getSize();
+    // Begin the OTA update process
     bool canBegin = Update.begin(contentLength);
     
     if (canBegin) {
       Serial.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quiet for a while.. Patience!");
 
-      // Retrieve the firmware .bin file and write it to flash
+      // Stream the firmware file from the server and store it to flash
       WiFiClient *updateStream = http.getStreamPtr();
       size_t written = Update.writeStream(*updateStream);
       
+      // Check if the full file has been written
       if (written == contentLength) {
         Serial.println("Written : " + String(written) + " successfully");
       } else {
         Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
       }
       
+      // End the OTA update process
       if (Update.end()) {
         Serial.println("OTA finished!");
+        // If update is completed, restart the ESP32
         if (Update.isFinished()) {
           Serial.println("Update successfully completed. Rebooting.");
           ESP.restart();
@@ -97,15 +107,18 @@ void OTA_UPDATE()
           Serial.println("Update not finished? Something went wrong!");
         }
       } else {
+        // If there was an error, print it
         Serial.println("Error Occurred. Error #: " + String(Update.getError()));
       }
     } else {
+      // If there wasn't enough space for OTA update, notify
       Serial.println("Not enough space for OTA Update");
     }
   } else {
+    // If the HTTP request failed for some other reason, print the error code
     Serial.println("Error on HTTP request");
   }
 
+  // Close the HTTP connection
   http.end();
 }
-
